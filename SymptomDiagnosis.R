e9efe43d720455a10c9d93d6d358b5859150bdde
Nelson Kamau <nelson.kaguamere@strathmore.edu>
2023-11-27 14:04:07 +0300

    created markdown and fixed the r code

--- SymptomDiagnosis.R
+++ SymptomDiagnosis.R
@@ -10,19 +10,6 @@ library(mice)
 # Read the CSV file with read.csv
 dataset <- read.csv("data/communicable_dataset.csv", na.strings = "NA")
 
-# Convert the "Disease" column to a factor with specified levels
-dataset$Disease <- factor(dataset$Disease, levels = c(
-  "Allergy", "GERD", "Chronic cholestasis", "Drug Reaction", "Peptic ulcer disease",
-  "AIDS", "Diabetes", "Gastroenteritis", "Bronchial Asthma", "Hypertension",
-  "Migraine", "Cervical spondylosis", "Paralysis (brain hemorrhage)", "Jaundice",
-  "Malaria", "Chicken pox", "Dengue", "Typhoid", "hepatitis A", "Hepatitis B",
-  "Hepatitis C", "Hepatitis D", "Hepatitis E", "Alcoholic hepatitis", "Tuberculosis",
-  "Common Cold", "Pneumonia", "Dimorphic hemmorhoids(piles)", "Heart attack",
-  "Varicose veins", "Hypothyroidism", "Hyperthyroidism", "Hypoglycemia", "Osteoarthristis",
-  "Arthritis", "(vertigo) Paroxysmal Positional Vertigo", "Acne", "Urinary tract infection",
-  "Psoriasis", "Impetigo"
-))
-
 # View the first few rows of the dataset
 head(dataset)
 
